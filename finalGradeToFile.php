5cb16639465c91e2348e2cddeedc1a98476aae4b
sgmalik <surya.malik@uvm.edu>
2024-04-09 12:24:22 -0400

    Logic added for final grade calculation, seems to be working, have to fix how output displays

--- finalGradeToFile.php
+++ finalGradeToFile.php
@@ -1,40 +1,69 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="River Bumpas and Surya Malik">
+        <title>Final Grade/ GPA Calculator</title>
+        <meta name="description" content="This page is for calculating a students final grade in a course and overall GPA.">
+        <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+    </head>
+    <body>
+    	<header>
+    		<h1>Final Grade and GPA Calculator</h1>
+            
+    	</header>
+
+        <nav class="menu">
+            <ul>
+                <li><a href="grade.php">Final Grade Calculator</a></li>
+                <li><a href="gpa.php">GPA Calculator</a></li>
+            </ul>
+        </nav>
+
+        <main>
+            <h2>
+                <?php
+            
+                    $data = "finalGrade" . "\n";
+
+                    $file = 'results.txt';
 
 
-$data = "finalGrade" . "\n";
-
-$file = 'results.txt';
-
-
-foreach ($_POST['courses'] as $course) {
-    $data .= $course['name'] . "\n";
+                    foreach ($_POST['courses'] as $course) {
+                        $data .= $course['name'] . "\n";
 
 
-    foreach ($course['categories'] as $category) {
-        $data .=  $category['name'] . "," . $category['weight'] . ",";
+                        foreach ($course['categories'] as $category) {
+                            $data .=  $category['name'] . "," . $category['weight'] . ",";
 
 
-        $numAssignments = count($category['assignments']);
+                            $numAssignments = count($category['assignments']);
 
-        foreach ($category['assignments'] as $assignmentIndex => $assignment) {
-            $data .= $assignment['name'] . "," . $assignment['score'];
+                            foreach ($category['assignments'] as $assignmentIndex => $assignment) {
+                                $data .= $assignment['name'] . "," . $assignment['score'];
 
-            if($assignmentIndex < $numAssignments){
-                $data .= ",";
-            }
-        }
-        $data .= "\n";
-    }
-    $data .= "\n";
-}
+                                if($assignmentIndex < $numAssignments){
+                                    $data .= ",";
+                                }
+                            }
+                            $data .= "\n";
+                        }
+                        $data .= "\n";
+                    }
 
 
-file_put_contents($file, '');
+                    file_put_contents($file, '');
 
-file_put_contents($file, $data, FILE_APPEND);
+                    file_put_contents($file, $data, FILE_APPEND);
 
-$python_script = 'process.py';
+                    $python_script = 'process.py';
 
-exec("python3 $python_script $file");
-?>
+                    $output = shell_exec("python3 $python_script $file");
+
+                    echo 'Your average(s) are: ' .$output;
+                ?>
+            </h2>
+        </main>  
+    </body>   
+</html>
 
--- process.py
+++ process.py
@@ -1,12 +1,14 @@
 import sys
 
-#Funciton to handle the backend logic for saving data and then doing calculations
+
+# Funciton to handle the backend logic for saving data and then doing calculations
 def handle(file_path):
     with open(file_path, "r") as file:
         path_choice = file.readline().strip()
-        
+
         if path_choice == "gpaCalculator":
-            #Saving gpa data to a list
+            type = "GPA"
+            # Saving gpa data to a list
             gpa_data = []
             for line in file:
                 info = line.strip().split(',')
@@ -14,22 +16,26 @@ def handle(file_path):
                 credits = int(info[1])
                 grade = int(info[2])
                 gpa_data.append((course, credits, grade))
-            return gpa_data
-        
-        #Logic for final grade, not yet working
+            return [gpa_data, type]
+
+        # Logic for final grade, not yet working
         elif path_choice == "finalGrade":
+            type = "GRADE"
             lines = file.readlines()
 
             course_name = ""
             courses = []
+            categories = []
             assignments = []
 
             for line in lines:
-                line = line.strip() 
+                line = line.strip()
 
                 # If the line is empty, go to next course
                 if not line:
+                    courses.append([course_name, categories])
                     course_name = ""
+                    categories = []
                     assignments = []
                     continue
 
@@ -40,17 +46,19 @@ def handle(file_path):
                 category_info = line.split(',')
                 category_name = category_info[0]
                 category_weight = float(category_info[1])
-                assignments = [(category_info[i], int(category_info[i+1])) for i in range(2, len(category_info)-1, 2)]
+                assignments = [(category_info[i], int(category_info[i + 1])) for i in
+                               range(2, len(category_info) - 1, 2)]
 
-                courses.append([course_name, [category_name, category_weight, assignments]])
-            return courses
+                categories.append([category_name, category_weight, assignments])
+            return [courses, type]
 
-#Function to calculate a GPA once given a list of formatted data
+
+# Function to calculate a GPA once given a list of formatted data
 def calculate_gpa(class_grades):
     total_credits = 0
     sum = 0.0
-    
-    #Calculating GPA with unpacked tuples from the input list
+
+    # Calculating GPA with unpacked tuples from the input list
     for name, credits, grade in class_grades:
         if 100 >= grade >= 93:
             sum += (4.0 * credits)
@@ -78,8 +86,27 @@ def calculate_gpa(class_grades):
             # Grade is less than 60, no points added to sum but credits counted towards total
             pass
         total_credits += credits
-    
+
     return sum / total_credits
+
+
+def calculate_final_grade(courses):
+    averages = []
+    numCourses = len(courses)
+    for i in range(0, numCourses):
+        totalWeightedAverage = 0.0
+        numCategories = len(courses[i][1])
+        for j in range(0, numCategories):
+            catWeight = courses[i][1][j][1]
+            weightedAverage = 0.0
+            totalPoints = 0.0
+            numAssignments = len(courses[i][1][j][2])
+            for k in range(0, numAssignments):
+                totalPoints = totalPoints + courses[i][1][j][2][k][1]
+            weightedAverage = (totalPoints / numAssignments) * catWeight
+            totalWeightedAverage = totalWeightedAverage + weightedAverage
+        averages.append(totalWeightedAverage)
+    return averages
 
 if __name__ == "__main__":
     # Making sure all command line arguments are passed
@@ -89,7 +116,10 @@ if __name__ == "__main__":
 
     # Getting the file name, and then outputting the resulting GPA
     file_name = sys.argv[1]
-    test = calculate_gpa(handle(file_name))
-    print(test)
+    test = handle(file_name)
+    if (test[1] == "GPA"):
+        print(calculate_gpa(test[0]))
+    elif (test[1] == "GRADE"):
+        print(calculate_final_grade(test[0]))
     
 
